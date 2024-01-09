6c4572a636c1dac27f41dc85f785a5bbd9bdb825
KanhaiyaKeshri <kanhaiyakeshri101@gmail.com>
2024-01-09 19:01:40 +0530

    Implementing functionality for users to add and delete notes

--- keeper-app/src/components/App.jsx
+++ keeper-app/src/components/App.jsx
@@ -1,24 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
-import notes from "../notes";
+import CreateArea from "./CreateArea";
 
 function App() {
+  const [noteArr, setNoteArr] = useState([]);
+  function addNote(inputNote) {
+    setNoteArr((prevValue) => {
+      return [...prevValue, inputNote];
+    });
+  }
+  function deleteNote(id) {
+    setNoteArr((prevNote) => {
+      return prevNote.filter((noteItem, index) => {
+        return index !== id;
+      });
+    });
+  }
   return (
     <div>
       <Header />
-      {notes.map((noteItem) => {
+      <CreateArea onClickAdd={addNote} />
+
+      {noteArr.map((note, index) => {
         return (
           <Note
-            key={noteItem.key}
-            title={noteItem.title}
-            content={noteItem.content}
+            key={index}
+            id={index}
+            title={note.title}
+            content={note.content}
+            onClickDelete={deleteNote}
           />
         );
       })}
+
       <Footer />
     </div>
   );
 }
+
 export default App;
